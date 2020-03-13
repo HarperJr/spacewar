340c16cc01fc7d44be4ac3bbe8f9fdf592b46720
Nikita Sychev <ns@digipeople.ru>
2020-03-13 21:52:26 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.11"
+}
+
+group = "net.harper"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+val log4jVersion = "2.12.1"
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+
+    implementation(fileTree("libs"))
+
+    implementation("org.apache.logging.log4j", "log4j-api", log4jVersion)
+    implementation("org.apache.logging.log4j", "log4j-core", log4jVersion)
+    implementation("com.google.code.gson", "gson", "2.8.6")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
